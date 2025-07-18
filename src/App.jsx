ac3f32979cb158ca67c08c74a5265bef5e8d1042
venkat sai <venkatsainayini.tech@gmail.com>
2025-07-19 02:13:19 +0530

    changed the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar/Navbar'
-import Intro from './assets/components/Intro/intro'
+import Intro from './assets/components/Intro/Intro'
 import Skills from './assets/components/Skills/skills'
 import Works from './assets/components/Works/works'
 import Contact from './assets/components/Contact/contact'
